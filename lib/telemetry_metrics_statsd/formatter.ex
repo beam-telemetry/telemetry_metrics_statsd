9539ca8b04da3f85d7ce5da6a62c95f2d7033739
Arkadiusz Gil <arkadiusz@arkgil.net>
2019-03-02 23:58:00 +0100

    Allow to configure global metric prefix

--- lib/telemetry_metrics_statsd/formatter.ex
+++ lib/telemetry_metrics_statsd/formatter.ex
@@ -4,16 +4,20 @@ defmodule TelemetryMetricsStatsd.Formatter do
   alias Telemetry.Metrics
 
   @spec format(
+          prefix :: String.t | nil,
           Telemetry.Metrics.t(),
           :telemetry.event_value(),
           tags :: [
             {Telemetry.Metrics.tag(), term()}
           ]
         ) :: binary()
-  def format(metric, value, tags) do
-    [format_metric_name(metric.name, tags), ?:, format_metric_value(metric, value)]
+  def format(prefix, metric, value, tags) do
+    [format_metric_name(prefix, metric.name, tags), ?:, format_metric_value(metric, value)]
     |> :erlang.iolist_to_binary()
   end
+
+  defp format_metric_name(nil, metric_name, tags), do: format_metric_name(metric_name, tags)
+  defp format_metric_name(prefix, metric_name, tags), do: format_metric_name([prefix | metric_name], tags)
 
   defp format_metric_name(metric_name, tags) do
     segments = metric_name ++ Enum.map(tags, fn {_, tag_value} -> tag_value end)
