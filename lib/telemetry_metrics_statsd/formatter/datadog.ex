26f53e0eea8cd9a0e882b5a01d2651f35681f7cc
Stephen Bussey <sb8244@cs.ship.edu>
2019-06-06 21:12:05 -0400

    Add DataDog formatter

---
+++ lib/telemetry_metrics_statsd/formatter/datadog.ex
@@ -0,0 +1,47 @@
+defmodule TelemetryMetricsStatsd.Formatter.Datadog do
+  @moduledoc false
+
+  @behaviour TelemetryMetricsStatsd.Formatter
+
+  alias Telemetry.Metrics
+
+  @impl true
+  def format(prefix, metric, value, tags) do
+    [
+      format_metric_name(prefix, metric.name),
+      ?:,
+      format_metric_value(metric, value),
+      format_metric_tags(tags)
+    ]
+    |> :erlang.iolist_to_binary()
+  end
+
+  defp format_metric_name(nil, metric_name), do: format_metric_name(metric_name)
+  defp format_metric_name(prefix, metric_name),
+    do: format_metric_name([prefix | metric_name])
+
+  defp format_metric_name(metric_name) do
+    metric_name
+    |> Enum.map(&to_string/1)
+    |> Enum.intersperse(?.)
+  end
+
+  defp format_metric_value(%Metrics.Counter{}, _value), do: "1|c"
+  defp format_metric_value(%Metrics.Summary{}, value), do: "#{value}|ms"
+  defp format_metric_value(%Metrics.Distribution{}, value), do: "#{value}|ms"
+  defp format_metric_value(%Metrics.LastValue{}, value), do: "#{value}|g"
+  defp format_metric_value(%Metrics.Sum{}, value) when value >= 0, do: "+#{value}|g"
+  defp format_metric_value(%Metrics.Sum{}, value), do: "#{value}|g"
+
+  defp format_metric_tags(tags), do: [?# | combine_tags(tags)]
+
+  defp combine_tags(tags) do
+    Enum.reduce(tags, [], fn
+      {k, v}, [] ->
+        [["#{k}:#{v}"]]
+      {k, v}, acc ->
+        [[?,, "#{k}:#{v}"] | acc]
+    end)
+    |> Enum.reverse()
+  end
+end
