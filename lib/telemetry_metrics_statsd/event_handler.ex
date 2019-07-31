0f79e2af612caa834ac522da2f6209cd930f5967
Connor Jacobsen <jacobsen.connor@gmail.com>
2019-07-31 03:34:16 -0400

    Global tags (#14)
    Global tags is a set of tag values that can be included in each metric
    handled by the reporter. A global tag needs to be enabled on a per-metric
    basis, just a any other tag.

--- lib/telemetry_metrics_statsd/event_handler.ex
+++ lib/telemetry_metrics_statsd/event_handler.ex
@@ -9,11 +9,12 @@ defmodule TelemetryMetricsStatsd.EventHandler do
           reporter :: pid(),
           mtu :: non_neg_integer(),
           prefix :: String.t() | nil,
-          formatter :: Formatter.t()
+          formatter :: Formatter.t(),
+          global_tags :: Keyword.t()
         ) :: [
           :telemetry.handler_id()
         ]
-  def attach(metrics, reporter, mtu, prefix, formatter) do
+  def attach(metrics, reporter, mtu, prefix, formatter, global_tags) do
     metrics_by_event = Enum.group_by(metrics, & &1.event_name)
 
     for {event_name, metrics} <- metrics_by_event do
@@ -25,7 +26,8 @@ defmodule TelemetryMetricsStatsd.EventHandler do
           metrics: metrics,
           mtu: mtu,
           prefix: prefix,
-          formatter: formatter
+          formatter: formatter,
+          global_tags: global_tags
         })
 
       handler_id
@@ -46,14 +48,18 @@ defmodule TelemetryMetricsStatsd.EventHandler do
         metrics: metrics,
         mtu: mtu,
         prefix: prefix,
-        formatter: formatter_mod
+        formatter: formatter_mod,
+        global_tags: global_tags
       }) do
     packets =
       for metric <- metrics do
         case fetch_measurement(metric, measurements) do
           {:ok, value} ->
             # The order of tags needs to be preserved so that the final metric name is built correctly.
-            tag_values = metric.tag_values.(metadata)
+            tag_values =
+              global_tags
+              |> Map.new()
+              |> Map.merge(metric.tag_values.(metadata))
             tags = Enum.map(metric.tags, &{&1, Map.fetch!(tag_values, &1)})
             Formatter.format(formatter_mod, metric, prefix, value, tags)
 
