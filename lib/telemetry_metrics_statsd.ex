6003703f154da52aa7a6ae9ca685acd5464283ad
haljin <paul.shinobi@gmail.com>
2020-10-18 15:48:14 +0200

    Docs

--- lib/telemetry_metrics_statsd.ex
+++ lib/telemetry_metrics_statsd.ex
@@ -366,6 +366,9 @@ defmodule TelemetryMetricsStatsd do
   * `:global_tags` - Additional default tag values to be sent along with every published metric. These
     can be overriden by tags sent via the `:telemetry.execute` call.
   * `:pool_size` - The number of UDP ports to open to report metrics. Defaults to `1`
+  * `:dns_polling_period` - When set makes `TelemetryMetricsStatsd` resolve the hostname of the StatsD server
+    on the specified interval. Defaults to `nil` disabling DNS resolution. This can improve performance of UDP
+    sending, while still allowing for dynamic resolution e.g. in a Kubernetes environment.
 
   You can read more about all the options in the `TelemetryMetricsStatsd` module documentation.
 
